7e869b1599a97c11e9113268161067e9a07f1ca1
Jon Shamir <jon.shamir@gmail.com>
2024-06-14 12:09:31 +0300

    Add logo component

---
+++ components/MainLogo/MainLogo.tsx
@@ -0,0 +1,44 @@
+import styles from "./MainLogo.module.scss";
+
+export function MainLogo({ isHidden }: { isHidden: boolean }) {
+  return (
+    <div className={styles.container + " " + styles.visible}>
+      <div className={!isHidden && styles.rotate}>
+        <div className={styles.logo}>
+          <div className={styles.top}>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+          </div>
+          <div className={styles.bottom}>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+          </div>
+          <div className={styles.middle}>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+          </div>
+          <div className={styles.side}>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+            <div className={styles.face}></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
