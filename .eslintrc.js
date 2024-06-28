a5e8956c676f9bf719ff1c30cd6c89169ff427e5
Jon Shamir <jon.shamir@gmail.com>
2024-06-28 11:56:51 +0300

    More project data

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
   extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
+    "plugin:mdx/recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
   ],
---
+++ components/VideoEmbed/VideoEmbed.js
@@ -0,0 +1,17 @@
+import styles from "./VideoEmbed.module.scss";
+
+export function VideoEmbed(props) {
+  return (
+    <div className={styles.VideoEmbed}>
+      <iframe
+        width="100%"
+        height="315"
+        src={`https://www.youtube.com/embed/${props.videoId}?`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="modestbranding; accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
