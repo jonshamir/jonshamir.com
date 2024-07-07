29ca1872d55ffbba743c0a778f59037df4c19fc0
Jon Shamir <jon.shamir@gmail.com>
2024-07-07 19:50:21 +0300

    Posthog config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,8 +7,8 @@ import "../styles/main.css";
 
 // Check that PostHog is client-side (used to handle Next.js SSR)
 if (typeof window !== "undefined") {
-  posthog.init("phc_Ud6pwqtRXUUeeC5zAjDoPZ7MYE41EdRWPMY2gdni1Yt", {
-    api_host: "https://app.posthog.com" || "https://us.i.posthog.com",
+  posthog.init(process.env.PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
     person_profiles: "identified_only",
     // Enable debug mode in development
     loaded: (posthog) => {
