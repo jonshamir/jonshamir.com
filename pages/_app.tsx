87cef1d95896e5a2a57b17f644b0c512b1a7a0cb
Jon Shamir <jon.shamir@gmail.com>
2024-07-07 19:22:24 +0300

    Add posthog analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,13 @@
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import posthog from "posthog-js";
 
 import "@fontsource-variable/work-sans";
 import "../styles/main.css";
+
+posthog.init("phc_Ud6pwqtRXUUeeC5zAjDoPZ7MYE41EdRWPMY2gdni1Yt", {
+  api_host: "https://app.posthog.com",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
