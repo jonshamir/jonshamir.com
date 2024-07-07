2dde7e81d759a72d8bed9403162c1160c0fd893d
Jon Shamir <jon.shamir@gmail.com>
2024-07-07 21:07:50 +0300

    Setup

---
+++ components/ThreeTest/ThreeTest.tsx
@@ -0,0 +1,13 @@
+export function ThreeTest() {
+  return (
+    <div
+      style={{
+        backgroundColor: "green",
+        color: "white",
+        padding: "1rem",
+      }}
+    >
+      ThreeTest
+    </div>
+  );
+}
