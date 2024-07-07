a0a516142b570a2e2d581f56c4b26fca0013b4ec
Jon Shamir <jon.shamir@gmail.com>
2024-07-07 19:14:35 +0300

    Add name to page titles

--- theme/constants.tsx
+++ theme/constants.tsx
@@ -5,4 +5,5 @@ export const DEFAULT_THEME: NextraBlogTheme = {
   darkMode: true,
   footer: <footer></footer>,
   readMore: "Read More →",
+  titleSuffix: " | Jon Shamir",
 };
--- theme/tags.tsx
+++ theme/tags.tsx
@@ -1,38 +1,38 @@
-import type { GetStaticPaths, GetStaticProps } from 'next'
-import Head from 'next/head'
-import { useSSG } from 'nextra/ssg'
-import { getStaticTags } from './utils/get-tags'
+import type { GetStaticPaths, GetStaticProps } from "next";
+import Head from "next/head";
+import { useSSG } from "nextra/ssg";
+import { getStaticTags } from "./utils/get-tags";
 
-const NEXTRA_INTERNAL = Symbol.for('__nextra_internal__')
+const NEXTRA_INTERNAL = Symbol.for("__nextra_internal__");
 
-export const TagTitle = ({ prefix = 'Posts tagged with ' }) => {
-  const { tag } = useSSG()
-  const title = `${prefix}${tag}`
+export const TagTitle = ({ prefix = "Posts tagged with " }) => {
+  const { tag } = useSSG();
+  const title = `${prefix}${tag}`;
   return (
     <Head>
       <title>{title}</title>
     </Head>
-  )
-}
+  );
+};
 
 export const TagName = () => {
-  const { tag } = useSSG()
-  return tag || null
-}
+  const { tag } = useSSG();
+  return tag || null;
+};
 
 export const getStaticPaths: GetStaticPaths = () => {
-  const tags = getStaticTags((globalThis as any)[NEXTRA_INTERNAL].pageMap)
+  const tags = getStaticTags((globalThis as any)[NEXTRA_INTERNAL].pageMap);
   return {
-    paths: tags.map(v => ({ params: { tag: v } })),
-    fallback: false
-  }
-}
+    paths: tags.map((v) => ({ params: { tag: v } })),
+    fallback: false,
+  };
+};
 export const getStaticProps: GetStaticProps = ({ params }) => {
   return {
     props: {
       ssg: {
-        tag: params?.tag
-      }
-    }
-  }
-}
+        tag: params?.tag,
+      },
+    },
+  };
+};
