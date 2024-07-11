5fb4f49da4c41ab87a70741cc30c0fef7448b94b
Jon Shamir <jon.shamir@gmail.com>
2024-07-11 23:47:17 +0300

    Cursor grab

--- components/ThreeTest/ThreeTest.tsx
+++ components/ThreeTest/ThreeTest.tsx
@@ -1,12 +1,11 @@
-import * as THREE from "three";
-import React, { useEffect, useRef, useState } from "react";
-import { Canvas, useFrame, ThreeElements } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { Scene } from "./Scene";
+
+import styles from "./Moon.module.scss";
 
 export function ThreeTest() {
   return (
-    <div style={{ width: "100%", height: "30rem" }}>
+    <div style={{ width: "100%", height: "30rem" }} className={styles.Moon}>
       <Canvas
         orthographic
         camera={{ zoom: 200, position: [0, 0, 10], far: 10 }}
