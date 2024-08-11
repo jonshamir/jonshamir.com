8fe2d2f16a37cde428b88208865a3cf4f672622f
Jon Shamir <jon.shamir@gmail.com>
2024-08-11 23:48:04 +0300

    Organization

--- theme/layouts/lab-layout.tsx
+++ theme/layouts/lab-layout.tsx
@@ -1,4 +1,3 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import type { ReactElement, ReactNode } from "react";
 import { useBlogContext } from "../blog-context";
@@ -6,6 +5,7 @@ import { MDXTheme } from "../mdx-theme";
 import { collectExperiments } from "../utils/collect";
 import getTags from "../utils/get-tags";
 import { BasicLayout } from "./basic-layout";
+import LabItem from "../../components/LabItem/LabItem";
 
 export function LabLayout({ children }: { children: ReactNode }): ReactElement {
   const { config, opts } = useBlogContext();
@@ -25,25 +25,15 @@ export function LabLayout({ children }: { children: ReactNode }): ReactElement {
     }
 
     const postTitle = post.frontMatter?.title || post.name;
-    const date: Date | null = post.frontMatter?.date
-      ? new Date(post.frontMatter.date)
-      : null;
     const coverImage = post.frontMatter?.cover;
 
     return (
-      <div key={post.route} className="lab-item">
-        <h3>
-          <Link href={post.route} passHref legacyBehavior>
-            <a>
-              {coverImage && <img src={coverImage} />}
-              <span>{postTitle}</span>
-            </a>
-          </Link>
-        </h3>
-        {date && (
-          <time dateTime={date.toISOString()}>{date.toDateString()}</time>
-        )}
-      </div>
+      <LabItem
+        key={post.route}
+        image={coverImage}
+        title={postTitle}
+        link={post.route}
+      />
     );
   });
   return (
