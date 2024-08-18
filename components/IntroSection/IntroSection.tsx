baa822957633690415cca8436ec8d80a61134237
Jon Shamir <jon.shamir@gmail.com>
2024-08-18 11:30:53 +0300

    Update summary

--- components/IntroSection/IntroSection.tsx
+++ components/IntroSection/IntroSection.tsx
@@ -12,7 +12,8 @@ export function IntroSection() {
         <h1>Hi, I&apos;m Jon Shamir</h1>
         <p>
           Design engineer & computer graphics expert. My interests include
-          interaction design, 3D graphics, data viz and machine learning.
+          interaction design, real-time 3D graphics, data viz and machine
+          learning.
         </p>
         <p>
           Currently building spatial interfaces at&nbsp;
