636a37d13ee441bac562f7b16baa65a266dec9f9
Jon Shamir <jon.shamir@gmail.com>
2024-08-30 22:12:02 +0300

    Rect

--- lab/Rect/index.tsx
+++ lab/Rect/index.tsx
@@ -1,14 +1,24 @@
 import { OrbitControls } from "@react-three/drei";
 import { ThreeCanvas } from "../../components/ThreeCanvas/ThreeCanvas";
+import * as THREE from "three";
+import { ThreeElements } from "@react-three/fiber";
+import { rectFragmentShader, rectVertexShader } from "./rect.glsl";
 
-function Quad(props: QuadProps) {
+type RectProps = ThreeElements["mesh"] & {
+  color?: THREE.Color;
+  depthTest?: boolean;
+  radius?: number;
+  shadow?: boolean;
+};
+
+function Rect(props: RectProps) {
   const { color = new THREE.Color(), depthTest = true, ...rest } = props;
   return (
     <mesh {...rest}>
       <planeGeometry args={[2, 2]} />
       <shaderMaterial
-        vertexShader={circleVertexShader}
-        fragmentShader={circleFragmentShader}
+        vertexShader={rectVertexShader}
+        fragmentShader={rectFragmentShader}
         depthTest={depthTest}
         depthWrite={false}
         transparent={true}
@@ -25,12 +35,12 @@ export default function RectCanvas() {
   return (
     <>
       <div style={{ width: "100%" }}>
-        <ThreeCanvas camera={{ position: [0, 0, 10], zoom: 3.5 }}>
+        <ThreeCanvas
+          camera={{ position: [0, 0, 10], zoom: 3.5 }}
+          style={{ backgroundColor: "grey" }}
+        >
           <OrbitControls enablePan={false} enableZoom={false} />
-          <mesh>
-            <planeGeometry args={[1, 32, 32]} />
-            <meshStandardMaterial color="orange" />
-          </mesh>
+          <Rect />
           <directionalLight position={[0, 0, 5]} intensity={1} />
         </ThreeCanvas>
       </div>
