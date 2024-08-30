25499df6488b8019c49e346b8dab00b0a24f6845
Jon Shamir <jon.shamir@gmail.com>
2024-08-30 17:53:55 +0300

    Cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,14 +20,6 @@ if (typeof window !== "undefined") {
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Head>
-        <link
-          rel="alternate"
-          type="application/rss+xml"
-          title="RSS"
-          href="/feed.xml"
-        />
-      </Head>
       <Component {...pageProps} />
     </>
   );
