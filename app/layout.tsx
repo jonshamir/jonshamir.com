e8570955bc08d8fad5552a79fccf092b172c8500
Jon Shamir <jon.shamir@gmail.com>
2024-09-01 18:06:39 +0300

    Start app refactoring

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+export const meta = {
+  title: "Jon Shamir",
+  description: "Jon Shamir portfolio website",
+  image: "https://jonshamir.com/profile.png",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <meta name="robots" content="follow, index" />
+        <meta name="description" content={meta.description} />
+        <meta property="og:site_name" content={meta.title} />
+        <meta property="og:description" content={meta.description} />
+        <meta property="og:title" content={meta.title} />
+        <meta property="og:image" content={meta.image} />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@jonshamir" />
+        <meta name="twitter:title" content={meta.title} />
+        <meta name="twitter:description" content={meta.description} />
+        <meta name="twitter:image" content={meta.image} />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ app/test/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <div>
+      <h1>Test Page</h1>
+      <p>This is a test page. It is a placeholder for future content.</p>
+    </div>
+  );
+}
