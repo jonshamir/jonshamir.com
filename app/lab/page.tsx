4b5378426e0fc312cbaa3009a5ed5cc1b3ce1b3d
Jon Shamir <jon.shamir@gmail.com>
2024-09-05 20:02:19 +0300

    Lab pagE

--- app/lab/page.tsx
+++ app/lab/page.tsx
@@ -1,13 +1,30 @@
 "use client";
 
-import { IntroSection } from "../../components/IntroSection/IntroSection";
-import { ProjectSection } from "../../components/ProjectSection/ProjectSection";
-import { SocialLinks } from "../../components/SocialLinks/SocialLinks";
+import LabItem from "../../components/LabItem/LabItem";
+
+const EXPERIMENTS = [
+  { title: "Rect", slug: "rect" },
+  { title: "Earth", slug: "earth" },
+  { title: "Craters", slug: "craters" },
+  { title: "Plant", slug: "plant" },
+];
 
 export default function Page() {
+  const experimentList = EXPERIMENTS.map((e) => {
+    return (
+      <LabItem
+        key={e.slug}
+        image={`/lab/${e.slug}.png`}
+        title={e.title}
+        link={`/lab/${e.slug}`}
+      />
+    );
+  });
+
   return (
     <>
       <p>Small projects, experiments & stuff</p>
+      <div style={{ display: "flex", gap: "1rem" }}>{experimentList}</div>
     </>
   );
 }
