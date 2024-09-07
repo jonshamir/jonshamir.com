98e876760d330448740bca7787f81aa0ae6a9a1c
Jon Shamir <jon.shamir@gmail.com>
2024-09-07 17:31:05 +0300

    Add email form

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,22 @@
 "use client";
 
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import { EmailForm } from "../components/EmailForm/EmailForm";
 import { IntroSection } from "../components/IntroSection/IntroSection";
 import { ProjectSection } from "../components/ProjectSection/ProjectSection";
 import { SocialLinks } from "../components/SocialLinks/SocialLinks";
+
+const queryClient = new QueryClient();
 
 export default function Page() {
   return (
     <>
       <IntroSection />
       <div className="MainPage">
+        <QueryClientProvider client={queryClient}>
+          <EmailForm />
+        </QueryClientProvider>
         <ProjectSection />
         <SocialLinks />
       </div>
