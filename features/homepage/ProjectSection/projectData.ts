730307d5356a3536e9997d6ec636d7a1dfa32387
Jon Shamir <jon.shamir@gmail.com>
2024-11-04 18:56:54 +0200

    Add spacetop

--- features/homepage/ProjectSection/projectData.ts
+++ features/homepage/ProjectSection/projectData.ts
@@ -8,6 +8,12 @@ type Project = {
 
 const projectData: Project[] = [
   {
+    slug: "spacetop",
+    name: "Spacetop",
+    subtitle: "2024 | AR Laptop",
+    color: "#e7e1e1"
+  },
+  {
     slug: "herb-tree",
     name: "Herb Tree",
     subtitle: "2023 | Interactive botanic visualization",
