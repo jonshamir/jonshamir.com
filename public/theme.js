18b4dc545bb3e3328e7c71fe95e1f0cedf09d28a
Jon Shamir <jon.shamir@gmail.com>
2024-11-09 23:16:25 +0200

    fix theme script

--- public/theme.js
+++ public/theme.js
@@ -1,53 +1,54 @@
-let timeoutAction;
-let timeoutEnable;
+(function () {
+  let timeoutAction;
+  let timeoutEnable;
 
-// Perform a task without any css transitions
-export const withoutTransition = (action) => {
-  // Clear fallback timeouts
-  clearTimeout(timeoutAction);
-  clearTimeout(timeoutEnable);
+  // Perform a task without any css transitions
+  function withoutTransition(action) {
+    // Clear fallback timeouts
+    clearTimeout(timeoutAction);
+    clearTimeout(timeoutEnable);
 
-  // Create style element to disable transitions
-  const style = document.createElement("style");
-  const css = document.createTextNode(`* {
+    // Create style element to disable transitions
+    const style = document.createElement("style");
+    const css = document.createTextNode(`* {
      -webkit-transition: none !important;
      -moz-transition: none !important;
      -o-transition: none !important;
      -ms-transition: none !important;
      transition: none !important;
   }`);
-  style.appendChild(css);
+    style.appendChild(css);
 
-  // Functions to insert and remove style element
-  const disable = () => document.head.appendChild(style);
-  const enable = () => document.head.removeChild(style);
+    // Functions to insert and remove style element
+    const disable = () => document.head.appendChild(style);
+    const enable = () => document.head.removeChild(style);
 
-  // Best method, getComputedStyle forces browser to repaint
-  if (typeof window.getComputedStyle !== "undefined") {
+    // Best method, getComputedStyle forces browser to repaint
+    if (typeof window.getComputedStyle !== "undefined") {
+      disable();
+      action();
+      window.getComputedStyle(style).opacity;
+      enable();
+      a;
+      return;
+    }
+
+    // Better method, requestAnimationFrame processes function before next repaint
+    if (typeof window.requestAnimationFrame !== "undefined") {
+      disable();
+      action();
+      window.requestAnimationFrame(enable);
+      return;
+    }
+
+    // Fallback
     disable();
-    action();
-    window.getComputedStyle(style).opacity;
-    enable();
-    return;
+    timeoutAction = setTimeout(() => {
+      action();
+      timeoutEnable = setTimeout(enable, 120);
+    }, 120);
   }
 
-  // Better method, requestAnimationFrame processes function before next repaint
-  if (typeof window.requestAnimationFrame !== "undefined") {
-    disable();
-    action();
-    window.requestAnimationFrame(enable);
-    return;
-  }
-
-  // Fallback
-  disable();
-  timeoutAction = setTimeout(() => {
-    action();
-    timeoutEnable = setTimeout(enable, 120);
-  }, 120);
-};
-
-(function () {
   try {
     withoutTransition(() => {
       var isDark = localStorage.getItem("color-scheme");
