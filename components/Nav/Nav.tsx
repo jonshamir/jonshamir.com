5e010d2c20c2dbdbd3a6bbdaa25e9d0e66c8c141
Jon Shamir <jon.shamir@gmail.com>
2024-11-16 12:11:36 +0200

    Transparent nav

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -25,30 +25,34 @@ export function Nav() {
   const logoRef = useRef(null);
 
   return (
-    isMounted && (
-      <nav className={styles.Nav}>
-        <div>
-          <Link href="/" passHref legacyBehavior>
-            <a style={{ padding: 0 }} ref={logoRef}>
-              <MainLogo parentRef={logoRef} />
-            </a>
-          </Link>
-
-          {NAV_ITEMS.map((item) => (
-            <Link href={item.href} passHref legacyBehavior key={item.href}>
-              <a
-                className={clsx("clickable", {
-                  [styles.ActiveLink]: pathname === item.href
-                })}
-              >
-                {item.title}
+    <>
+      {isMounted && (
+        <nav className={styles.Nav}>
+          <div>
+            <Link href="/" passHref legacyBehavior>
+              <a style={{ padding: 0 }} ref={logoRef}>
+                <MainLogo parentRef={logoRef} />
               </a>
             </Link>
-          ))}
-          <div style={{ flexGrow: 1 }} />
-          <ThemeToggle />
-        </div>
-      </nav>
-    )
+            <div style={{ flexGrow: 1 }} />
+
+            <div className={styles.Wrapper}>
+              {NAV_ITEMS.map((item) => (
+                <Link href={item.href} passHref legacyBehavior key={item.href}>
+                  <a
+                    className={clsx("clickable", {
+                      [styles.ActiveLink]: pathname === item.href
+                    })}
+                  >
+                    {item.title}
+                  </a>
+                </Link>
+              ))}
+              <ThemeToggle />
+            </div>
+          </div>
+        </nav>
+      )}
+    </>
   );
 }
