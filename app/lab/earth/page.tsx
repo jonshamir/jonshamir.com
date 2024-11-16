a0a70d5ad03ae324b56b817ed656277959a9d338
Jon Shamir <jon.shamir@gmail.com>
2024-11-16 17:02:41 +0200

    More on earth

--- app/lab/earth/page.tsx
+++ app/lab/earth/page.tsx
@@ -13,6 +13,22 @@ export default function Page() {
       </p>
       <p>Click the shapes to see the Earth projected onto them:</p>
       <Canvas />
+      <p>
+        <br />
+        Usually textures are mapped to 3d meshes using explicit uv mapping -
+        each vertex is assigned specific uv (texture-space) coordinates.
+        Projection mapping is a texture mapping technique that calculates uv
+        coordinates dynamically from vertex positions.
+      </p>
+      <p>
+        You might notice a little seam on one side of the Earth. This is due to
+        the way the uv coordinates are calculated. The problem is described in
+        detail in{" "}
+        <a href="https://bgolus.medium.com/distinctive-derivative-differences-cce38d36797b">
+          Distinctive Derivative Differences
+        </a>{" "}
+        Pesky Problems with Procedural UVs
+      </p>
     </>
   );
 }
