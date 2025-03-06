17e63ca87ece61dc2f758f710f062ea3b5081119
Jon Shamir <jon.shamir@gmail.com>
2025-03-06 21:09:53 +0200

    More new design

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,6 @@ import { InlineLogoPlaceholder } from "../components/Logo/InlineLogoPlaceholder"
 import projectData from "../data/projects";
 import { SOCIAL_LINKS } from "../data/social";
 import { EmailForm } from "../features/homepage/EmailForm/EmailForm";
-import { FluidSim } from "../features/homepage/FluidSim/FluidSim";
 import styles from "./page.module.css";
 
 const queryClient = new QueryClient();
@@ -16,7 +15,6 @@ const queryClient = new QueryClient();
 export default function Page() {
   return (
     <>
-      {/* <FluidSim /> */}
       <div className={styles.IntroSection}>
         <InlineLogoPlaceholder />
         <h1>Hi, I&rsquo;m Jon Shamir</h1>
@@ -40,7 +38,12 @@ export default function Page() {
           ))}
         </div>
       </div>
-      <h2>Projects</h2>
+      <h3>Projects</h3>
+      <p>
+        I&rsquo;m a product engineer and designer. My interests include
+        interaction design, real-time 3D graphics, data visualization and
+        machine learning.
+      </p>
       {projectData.map((project) => (
         <Link
           href={`projects/${project.slug}`}
