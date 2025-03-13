f8646cc710ec365e257c6d0138ca399cf0ff1bbb
Jon Shamir <jon.shamir@gmail.com>
2025-03-13 19:11:39 +0200

    More spacetop content

--- app/projects/spacetop/page.tsx
+++ app/projects/spacetop/page.tsx
@@ -12,34 +12,114 @@ export default function Page() {
           className="button"
           rel="noreferrer"
         >
-          Visit Project <span className="arrow">↗</span>
+          Official Spacetop site <span className="arrow">↗</span>
         </a>
       </p>
       <p>
-        Spacetop is a spatial operating system designed to work with AR glasses
-        to replace physical external monitors while on the go. Translating the
-        traditional desktop user interface to a 3D environment provided many
-        challenges, both in design and implementation.{" "}
+        Spacetop is a spatial operating system, designed to work with a
+        keyboard, mouse and augmented reality glasses instead of a screen.
+        Unlike other XR products, Spacetop has a minimal learning curve and is
+        intuitive to first-time users because it builds upon traditional desktop
+        interaction paradigms.
       </p>
 
       <figure className="full-bleed">
         <img
           src="/projects/spacetop/hero.png"
-          alt="Leaves sorted by shape"
+          alt="Illustration of Spacetop being used on an airplane"
           style={{ maxHeight: "450px", height: "auto", maxWidth: "1000px" }}
         />
         <figcaption>
-          A rendering demonstrating the use of Spacetop - the windows float
-          around the user in 3D space, while the environment is visible
+          In Spacetop, windows are overlaid on top of the real world, allowing
+          users to interact with them using a keyboard, mouse and augmented
+          reality glasses.
         </figcaption>
       </figure>
 
       <p>
-        Spacetop is a spatial operating system designed to work with AR glasses
-        to replace physical external monitors while on the go. Translating the
-        traditional desktop user interface to a 3D environment provided many
-        challenges, both in design and implementation.{" "}
+        Translating the traditional desktop user interface to a 3D environment
+        provided many challenges, both in design and implementation. My work
+        involved prototyping novel spatial interactions, defining and developing
+        frameworks to manage UI in 3D space, creating special shader effects and
+        more.
       </p>
+      <h2>The Canvas</h2>
+      <p>
+        The canvas is a huge virtual &quot;screen&quot; that curves around the
+        user. Instead of being limited by the edges of a physical screen, the
+        virtual canvas can expand to fit the content. To allow the users to
+        understand the 3D shape of the canvas surface without being
+        claustrophobic, a dot grid is revealed only around the cursor and
+        content.
+      </p>
+      <figure>
+        <img
+          src="/projects/spacetop/cursor-move.gif"
+          alt="The cursor moving around on the canvas"
+          style={{ maxHeight: "450px", height: "auto", maxWidth: "1000px" }}
+        />
+      </figure>
+      <h2>Spaces</h2>
+      <p>
+        What does it mean for a dialog or pop-up to be in 3D space? How can we
+        ensure the user understands what's going on without losing context or
+        missing a notification? The system UI structure, its layers, their roles
+        and mechanics: how dialogs work, where do notifications pop up, how is
+        input focus communicated, etc.
+      </p>
+      <p>
+        Created a framework for managing user interfaces in 3D, integrating data
+        from various sensors into a performant and easy to use API. Worked
+        closely with the algorithms team to incorporate computer vision models
+        into the product.
+      </p>
+      <figure>
+        <img
+          src="/projects/spacetop/compass.gif"
+          alt="The compass points the user to a window outside the field of view"
+          style={{ maxHeight: "450px", height: "auto", maxWidth: "1000px" }}
+        />
+        <figcaption>
+          The compass points the user to a window outside the field of view.
+        </figcaption>
+      </figure>
+
+      <figure className="full-bleed">
+        <img
+          src="/projects/spacetop/nav.gif"
+          alt="The compass points the user to a window outside the field of view"
+          style={{ maxHeight: "450px", height: "auto", maxWidth: "1000px" }}
+        />
+        <figcaption>
+          The canvas is dynamic and can be navigated by the user.
+        </figcaption>
+      </figure>
+
+      <h2>Materials & Rendering</h2>
+      <p>
+        Developed custom shaders designed for XR rendering, taking into account
+        performance and legibility while implementing spatial UI effects unique
+        to the 3D medium. 3D user interfaces allow us to utilize material
+        properties that we do not have in 2d interfaces.
+      </p>
+      <figure>
+        <img
+          src="/projects/spacetop/bevel.gif"
+          alt="A special shader that created a 3D illusion"
+          style={{ maxHeight: "450px", height: "auto", maxWidth: "1000px" }}
+        />
+      </figure>
+
+      <figure>
+        <img
+          src="/projects/spacetop/widgets.gif"
+          alt="Widgets demonstrate the use of materials and rendering techniques."
+          style={{ maxHeight: "450px", height: "auto", maxWidth: "1000px" }}
+        />
+        <figcaption>
+          Widgets demonstrate the use of materials and rendering techniques.
+        </figcaption>
+      </figure>
     </>
   );
 }
