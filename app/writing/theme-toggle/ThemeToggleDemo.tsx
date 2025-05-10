d73edc40caef3dbeb2c0f55743d7338ef89d0aca
Jon Shamir <jon.shamir@gmail.com>
2025-05-10 20:33:33 +0300

    Add theme page

---
+++ app/writing/theme-toggle/ThemeToggleDemo.tsx
@@ -0,0 +1,9 @@
+import styles from "./ThemeToggleDemo.module.css";
+
+export function ThemeToggleDemo() {
+  return (
+    <div className={styles.ThemeToggleDemo}>
+      <div />
+    </div>
+  );
+}
