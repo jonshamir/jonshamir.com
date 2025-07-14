2ad86b42192e223ae31ec84bd1bb2ce1ae644ced
Jon Shamir <jon.shamir@gmail.com>
2025-07-14 23:25:59 +0300

    Changing videos

--- src/components/SelectedWork/SelectedWork.tsx
+++ src/components/SelectedWork/SelectedWork.tsx
@@ -1,12 +1,28 @@
+"use client";
+
+import { useState } from "react";
+
+const videos = [
+  "homepage/herbs.mp4",
+  "homepage/earth.mp4",
+  "homepage/muser.mp4"
+];
+
 export function SelectedWork() {
+  const [currentVideoIndex, setCurrentVideoIndex] = useState(0);
+
+  const handleVideoEnd = () => {
+    setCurrentVideoIndex((prevIndex) => (prevIndex + 1) % videos.length);
+  };
+
   return (
     <figure className="full-bleed">
       <video
-        src="homepage/earth.mp4"
+        src={videos[currentVideoIndex]}
         autoPlay
         muted
-        loop
         playsInline
+        onEnded={handleVideoEnd}
         style={{
           maxWidth: "1000px",
           maxHeight: "500px",
