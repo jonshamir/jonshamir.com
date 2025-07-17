f8f6e2d664b2f6d1bf94f66426224546d1069246
Jon Shamir <jon.shamir@gmail.com>
2025-07-17 14:18:30 +0300

    Nicer code styling

--- next.config.mjs
+++ next.config.mjs
@@ -32,7 +32,20 @@ const withMDX = nextMDX({
       [rehypePrettyCode, {
         theme: "github-dark",
         defaultLang: "plaintext",
-        grid: false
+        grid: false,
+        onVisitLine(node) {
+          // Prevent lines from collapsing in `display: grid` mode, and allow empty
+          // lines to be copy/pasted
+          if (node.children.length === 0) {
+            node.children = [{ type: 'text', value: ' ' }];
+          }
+        },
+        onVisitHighlightedLine(node) {
+          node.properties.className.push('line--highlighted');
+        },
+        onVisitHighlightedChars(node) {
+          node.properties.className.push('chars--highlighted');
+        }
       }]
     ],
     format: "mdx"
