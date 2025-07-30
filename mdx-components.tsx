5c257afd8ef382759dd24b0fac0c9d2db40bfde7
Jon Shamir <jon.shamir@gmail.com>
2025-07-30 22:11:45 +0300

    Outlinks

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,33 @@
 import type { MDXComponents } from "mdx/types";
 
+import { Outlink } from "./src/components/Outlink";
+
+function isExternalLink(href: string): boolean {
+  return href.startsWith("http://") || href.startsWith("https://");
+}
+
+function CustomLink({
+  href,
+  children,
+  ...props
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  if (isExternalLink(href)) {
+    return <Outlink href={href}>{children}</Outlink>;
+  }
+
+  return (
+    <a href={href} {...props}>
+      {children}
+    </a>
+  );
+}
+
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
-    ...components
+    ...components,
+    a: CustomLink
   };
 }
