0e1e7229328a078bac7fbc84eb3fd7a64c918c1e
Jon Shamir <jon.shamir@gmail.com>
2025-07-30 23:14:42 +0300

    fix errs

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,25 +1,22 @@
 import type { MDXComponents } from "mdx/types";
 
-import { Outlink } from "./src/components/Outlink";
-
 function isExternalLink(href: string): boolean {
   return href.startsWith("http://") || href.startsWith("https://");
 }
 
-function CustomLink({
-  href,
-  children,
-  ...props
-}: {
-  href: string;
-  children: React.ReactNode;
-}) {
-  if (isExternalLink(href)) {
-    return <Outlink href={href}>{children}</Outlink>;
-  }
+function CustomLink(props: React.ComponentProps<"a">): JSX.Element {
+  const { href, children, className, ...rest } = props;
 
   return (
-    <a href={href} {...props}>
+    <a
+      href={href}
+      className={
+        href && isExternalLink(href)
+          ? `outlink ${className || ""}`.trim()
+          : className
+      }
+      {...rest}
+    >
       {children}
     </a>
   );
@@ -28,6 +25,7 @@ function CustomLink({
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    a: CustomLink
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-assignment
+    a: CustomLink as any
   };
 }
