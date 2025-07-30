9ce1b60c2406e7e0d01745aa86d1b8a95f058234
Jon Shamir <jon.shamir@gmail.com>
2025-07-31 00:04:48 +0300

    Add import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { InlineLogoPlaceholder } from "../components/Logo/InlineLogoPlaceholder";
+import { PostList } from "../components/PostList/PostList";
 import { ProjectList } from "../components/ProjectList/ProjectList";
 import { SelectedWork } from "../components/SelectedWork/SelectedWork";
 import { SocialLinks } from "../components/SocialLinks/SocialLinks";
