dd32f574b59159bc44fa46eec5cf86412e3b0c55
Jon Shamir <jon.shamir@gmail.com>
2025-07-31 15:36:49 +0300

    Copy button improvements

--- src/features/mdx/components/CopyButton.tsx
+++ src/features/mdx/components/CopyButton.tsx
@@ -10,13 +10,16 @@ interface CopyButtonProps {
 
 export const CopyButton = ({ text }: CopyButtonProps) => {
   const [isCopied, setIsCopied] = useState(false);
+  const [showCheckmark, setShowCheckmark] = useState(false);
 
   const copy = () => {
     navigator.clipboard
       .writeText(text)
       .then(() => {
         setIsCopied(true);
+        setShowCheckmark(true);
         setTimeout(() => setIsCopied(false), 3000);
+        setTimeout(() => setShowCheckmark(false), 3500);
       })
       .catch((err) => {
         console.error("Failed to copy text: ", err);
@@ -28,9 +31,9 @@ export const CopyButton = ({ text }: CopyButtonProps) => {
       className="copy-button"
       disabled={isCopied}
       onClick={copy}
-      aria-label={isCopied ? "Copied!" : "Copy code to clipboard"}
+      aria-label={isCopied ? "Copied!" : "Copy code"}
     >
-      {isCopied ? (
+      {showCheckmark ? (
         <FeatherIcon iconName="check" size={16} />
       ) : (
         <FeatherIcon iconName="copy" size={16} />
