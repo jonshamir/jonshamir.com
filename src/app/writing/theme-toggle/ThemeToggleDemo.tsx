e8a1c4a56d144737905428ba77db16ca98ae668a
Jon Shamir <jon.shamir@gmail.com>
2025-08-18 20:20:21 +0300

    Simpler implementation

--- src/app/writing/theme-toggle/ThemeToggleDemo.tsx
+++ src/app/writing/theme-toggle/ThemeToggleDemo.tsx
@@ -5,13 +5,13 @@ import { ReactNode, useState } from "react";
 import styles from "./ThemeToggleDemo.module.css";
 
 type ThemeToggleDemoProps = {
-  showFancyToggle: boolean;
+  showToggle: boolean;
   isAnimated: boolean;
   children: ReactNode;
 };
 
 export function ThemeToggleDemo({
-  showFancyToggle,
+  showToggle,
   isAnimated,
   children
 }: ThemeToggleDemoProps) {
@@ -22,45 +22,33 @@ export function ThemeToggleDemo({
         className={`${styles.ThemeToggleDemo} ${isDark ? styles.dark : ""}  ${isAnimated ? styles.animated : ""}`}
       >
         {children}
-        {!showFancyToggle && (
+        {!showToggle && (
           <button onClick={() => setIsDark(!isDark)}>
             {isDark ? "Set Dark Mode" : "Set Light Mode"}
           </button>
         )}
-        {showFancyToggle && (
-          <button
-            className={`${styles.ThemeToggle}`}
-            onClick={() => setIsDark(!isDark)}
-          />
+        {showToggle && (
+          <button onClick={() => setIsDark(!isDark)}>
+            <svg
+              className="theme-toggle"
+              width="48"
+              height="48"
+              viewBox="0 0 24 24"
+            >
+              <circle
+                className="base"
+                cx="12"
+                cy="12"
+                r="7.9"
+                mask="url(#half-mask)"
+                stroke="currentColor"
+                strokeWidth="2"
+              />
+              <path className={styles.LeftSide} d="M 12 5 A 7 7 0 0 1 12 19" />
+              <path className={styles.RightSide} d="M 12 5 A 7 7 0 0 1 12 19" />
+            </svg>
+          </button>
         )}
-        <svg
-          className="theme-toggle"
-          width="48"
-          height="48"
-          viewBox="0 0 24 24"
-        >
-          <circle
-            className="base"
-            cx="12"
-            cy="12"
-            r="8"
-            mask="url(#mask)"
-            fill="currentColor"
-          />
-
-          <mask id="mask">
-            <rect x="0" y="0" width="100%" height="100%" fill="white" />
-            <path
-              className="maskPath"
-              d="M 12 5 A 7 7 0 0 1 12 19"
-              fill="black"
-              style={{
-                transformOrigin: "center",
-                transform: "rotateY(180deg)"
-              }}
-            />
-          </mask>
-        </svg>
       </div>
     </figure>
   );
