62f6e199ea0238a7b21271327af11e6f6ac596b5
Jon Shamir <jon.shamir@gmail.com>
2025-08-19 22:33:57 +0300

    Nicer css

--- src/app/writing/theme-toggle/ThemeToggleDemo.tsx
+++ src/app/writing/theme-toggle/ThemeToggleDemo.tsx
@@ -38,7 +38,7 @@ export function ThemeToggleDemo({
               <circle
                 cx="12"
                 cy="12"
-                r="7.85"
+                r="8"
                 stroke="currentColor"
                 strokeWidth="2"
                 fill="none"
