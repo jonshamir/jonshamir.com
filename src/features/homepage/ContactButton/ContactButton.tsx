7477f1818d6cf5f070b7660698a6958c6b836126
Jon Shamir <jon.shamir@gmail.com>
2026-03-24 17:47:49 +0200

    meta

--- src/features/homepage/ContactButton/ContactButton.tsx
+++ src/features/homepage/ContactButton/ContactButton.tsx
@@ -5,13 +5,42 @@ import styles from "./ContactButton.module.css";
 
 export function ContactButton() {
   return (
-    <ButtonLink
-      round
-      variant="primary"
-      href="mailto:jon@studio-normal.com"
-      className={styles.contactButton}
+    <div
+      className={styles.contactButtonWrapper}
+      style={{ filter: "url(#meta)" }}
     >
-      Contact<span className="arrow">↗</span>
-    </ButtonLink>
+      <svg xmlns="http://www.w3.org/2000/svg" className={styles.hidden}>
+        <defs>
+          <filter id="meta">
+            <feGaussianBlur in="SourceGraphic" stdDeviation="3" result="blur" />
+            <feColorMatrix
+              in="blur"
+              mode="matrix"
+              values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -6"
+              result="meta"
+            />
+            <feBlend in="SourceGraphic" in2="meta" />
+          </filter>
+        </defs>
+      </svg>
+      <ButtonLink
+        round
+        variant="primary"
+        href="mailto:jon@studio-normal.com"
+        className={styles.contactButton}
+      >
+        <span className={styles.contactButtonText}>Copy Address</span>
+      </ButtonLink>
+      <ButtonLink
+        round
+        variant="primary"
+        href="mailto:jon@studio-normal.com"
+        className={styles.contactButton}
+      >
+        <span className={styles.contactButtonText}>
+          Email Me<span className="arrow">↗</span>
+        </span>
+      </ButtonLink>
+    </div>
   );
 }
