df884bd673207a13b6594784992933bf981086e4
Jon Shamir <jon.shamir@gmail.com>
2026-03-25 17:35:52 +0200

    img

--- src/features/homepage/ProjectScroller/ProjectScroller.tsx
+++ src/features/homepage/ProjectScroller/ProjectScroller.tsx
@@ -1,26 +1,23 @@
 "use client";
 
 import { SideScroller } from "../../../components/SideScroller/SideScroller";
+import styles from "./ProjectScroller.module.css";
 
 export function ProjectScroller() {
   return (
     <SideScroller>
-      {Array.from({ length: 5 }, (_, i) => (
-        <div
-          key={i}
-          style={{
-            width: "30rem",
-            background: "var(--color-accent)",
-            padding: "2rem",
-            minHeight: "400px",
-            display: "grid",
-            placeItems: "center",
-            borderRadius: "var(--rounding-medium)"
-          }}
-        >
-          <h2>Slide {i + 1}</h2>
-        </div>
-      ))}
+      <div className={styles.item}>
+        <img src="/projects/spacetop/hero.png" alt="Project 1" />
+      </div>
+      <div className={styles.item}>
+        <h2>Slide 2</h2>
+      </div>
+      <div className={styles.item}>
+        <h2>Slide 3</h2>
+      </div>
+      <div className={styles.item}>
+        <h2>Slide 4</h2>
+      </div>
     </SideScroller>
   );
 }
