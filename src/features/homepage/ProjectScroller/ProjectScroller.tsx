1c146aba6a0e615607ef832e7151db0fc6ae1740
Jon Shamir <jon.shamir@gmail.com>
2026-03-25 18:30:45 +0200

    color

--- src/features/homepage/ProjectScroller/ProjectScroller.tsx
+++ src/features/homepage/ProjectScroller/ProjectScroller.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import clsx from "clsx";
+
 import { SideScroller } from "../../../components/SideScroller/SideScroller";
 import styles from "./ProjectScroller.module.css";
 
@@ -7,16 +9,48 @@ export function ProjectScroller() {
   return (
     <SideScroller>
       <div className={styles.item}>
-        <img src="/projects/spacetop/hero.png" alt="Spacetop" />
+        <div className={styles.itemContent}>
+          <h2>Spacetop</h2>
+          <p>Augmented reality laptop OS</p>
+        </div>
+        <img
+          src="/projects/spacetop/hero.png"
+          alt="Spacetop"
+          className={styles.itemMedia}
+        />
       </div>
       <div className={styles.item}>
-        <img src="/projects/muser/hero.png" alt="Muser" />
+        <div className={styles.itemContent}>
+          <h2>Muser</h2>
+          <p>Smart music visualizer</p>
+        </div>
+        <img
+          src="/projects/muser/hero.png"
+          alt="Muser"
+          className={styles.itemMedia}
+        />
       </div>
       <div className={styles.item}>
-        <img src="/projects/widgets/screenshot.png" alt="Widgets Bar" />
+        <div className={clsx(styles.itemContent, styles.darkContent)}>
+          <h2>Widgets Bar</h2>
+          <p>Customizable widgets bar</p>
+        </div>
+        <img
+          src="/projects/widgets/screenshot.png"
+          alt="Widgets Bar"
+          className={styles.itemMedia}
+        />
       </div>
       <div className={styles.item}>
-        <img src="/projects/leaf-map/hero.png" alt="Leaf Map" />
+        <div className={clsx(styles.itemContent, styles.darkContent)}>
+          <h2>Leaf Map</h2>
+          <p>Interactive botanical visualization</p>
+        </div>
+        <img
+          src="/projects/leaf-map/hero.png"
+          alt="Leaf Map"
+          className={styles.itemMedia}
+        />
       </div>
     </SideScroller>
   );
