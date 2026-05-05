ca501b8bf1865881c156710bd650fd0a267f2a56
Jon Shamir <jon.shamir@gmail.com>
2026-05-05 17:18:50 +0300

    size

--- src/app/lab/shaper/page.tsx
+++ src/app/lab/shaper/page.tsx
@@ -5,7 +5,6 @@ import styles from "./page.module.css";
 export default function Page() {
   return (
     <>
-      <h1>Shaper</h1>
       <div className={styles.container}>
         <p className={styles.time}>09:34</p>
       </div>
