33451b4f7dc9c346a11021ddbea3cfd31b763dcf
Jon Shamir <jon.shamir@gmail.com>
2026-08-03 15:52:41 +0300

    project previews

--- src/components/ProjectList/ProjectList.tsx
+++ src/components/ProjectList/ProjectList.tsx
@@ -1,25 +1,38 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
-import { createElement } from "react";
+import { createElement, useState } from "react";
 
-import heroImg from "../../app/projects/simply/hero.jpg";
 import styles from "./ProjectList.module.css";
 import projectData from "./projects";
 
 export default function ProjectList() {
+  const [hovered, setHovered] = useState<string | null>(null);
+
   return (
     <ul className={styles.ProjectList}>
       {projectData.map((project) => (
-        <li key={project.slug}>
+        <li
+          key={project.slug}
+          onMouseEnter={() => setHovered(project.slug)}
+          onMouseLeave={() => setHovered(null)}
+        >
           <Link
             href={`/projects/${project.slug}`}
             className={styles.projectLink}
+            style={hovered === project.slug ? { zIndex: 2 } : undefined}
           >
             {createElement(project.icon)}
             <strong className={styles.name}>{project.name}</strong>
             <span className={styles.subtitle}>{project.subtitle}</span>
             <div className={styles.preview}>
-              <Image src={heroImg} alt="Illustration of Simply Piano XR" />
+              <Image
+                src={project.preview}
+                alt=""
+                loading="eager"
+                fetchPriority="low"
+              />
             </div>
           </Link>
         </li>
